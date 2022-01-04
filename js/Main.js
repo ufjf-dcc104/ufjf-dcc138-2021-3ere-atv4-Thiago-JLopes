cdd9d2161503108ba0e2e87b6c0b2d69a403e4d3
Thiago José Lopes <thiago.jose@estudante.ufjf.br>
2022-01-03 23:39:52 -0300

    Adiciona exemplo de uso de audio básico.

--- js/Main.js
+++ js/Main.js
@@ -11,27 +11,28 @@ assets.carregaImagem("garota", "assets/garota.png");
 assets.carregaImagem("esqueleto", "assets/skelly.png");
 assets.carregaImagem("orc", "assets/orc.png");
 assets.carregaAudio("moeda", "assets/coin.wav");
+assets.carregaAudio("boom", "assets/boom.wav");
 
 const canvas = document.querySelector("canvas");
-canvas.width = 14*32;
-canvas.height = 10*32;
+canvas.width = 14 * 32;
+canvas.height = 10 * 32;
 const cena1 = new Cena(canvas, assets);
 
 const mapa1 = new Mapa(10, 14, 32);
 mapa1.carregaMapa(modeloMapa1);
 cena1.configuraMapa(mapa1);
 
-const pc = new Sprite({x:50 ,y: 150,vx: 10,});
-const en1 = new Sprite({x:160, vx: -10, color:"red"});
+const pc = new Sprite({ x: 50, y: 150, vx: 10, });
+const en1 = new Sprite({ x: 160, vx: -10, color: "red" });
 
 cena1.adicionar(pc);
 cena1.adicionar(en1);
-cena1.adicionar(new Sprite({x: 115, y:70, vy: 10, color:"red"}));
-cena1.adicionar(new Sprite({x: 115, y:160, vy: -10, color:"red"}));
+cena1.adicionar(new Sprite({ x: 115, y: 70, vy: 10, color: "red" }));
+cena1.adicionar(new Sprite({ x: 115, y: 160, vy: -10, color: "red" }));
 
 cena1.iniciar();
 
-document.addEventListener("keydown", (e)=> {
+document.addEventListener("keydown", (e) => {
     switch (e.key) {
         case "s":
             cena1.iniciar();
@@ -41,6 +42,9 @@ document.addEventListener("keydown", (e)=> {
             break;
         case "c":
             assets.audio("moeda").play();
+            break;
+        case "b":
+            assets.audio("boom").play();
             break;
     }
 })
