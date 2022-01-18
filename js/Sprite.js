9936dd7225142e115d08dbb20b79cf2dda0df402
Thiago José Lopes <thiago.jose@estudante.ufjf.br>
2022-01-18 09:00:06 -0300

    Adiciona controlador por entrada em pc.

--- js/Sprite.js
+++ js/Sprite.js
@@ -21,11 +21,18 @@ export default class Sprite {
             this.cena.mapa.SIZE,
             this.cena.mapa.SIZE)
     }
-    passo(dt) {
+    controlar(dt){
+
+    }
+    mover(dt){
         this.x = this.x + this.vx * dt;
         this.y = this.y + this.vy * dt;
         this.mx = Math.floor(this.x / this.cena.mapa.SIZE);
         this.my = Math.floor(this.y / this.cena.mapa.SIZE);
+    }
+    passo(dt) {
+        this.controlar(dt);
+        this.mover(dt);
     }
     colidiuCom(outro) {
         return !((this.x - this.w / 2 > outro.x + outro.w / 2) || (this.x + this.w / 2 < outro.x - outro.w / 2) || (this.y - this.h / 2 > outro.y + outro.h / 2) || (this.y + this.h / 2 < outro.y - outro.h / 2));
