15500b7602554f12ce4f1e27197dbf9c800529cb
Thiago José Lopes <thiago.jose@estudante.ufjf.br>
2022-01-18 09:35:14 -0300

    Adiciona persiguição nos inimigos.

--- js/Sprite.js
+++ js/Sprite.js
@@ -1,6 +1,6 @@
 export default class Sprite {
     /*É responsável por modelar algo que se move na tela.*/
-    constructor({ x = 100, y = 100, w = 20, h = 20, color = "white", vx = 0, vy = 0 } = {}) {
+    constructor({ x = 100, y = 100, w = 20, h = 20, color = "white", vx = 0, vy = 0, controlar = ()=> {}} = {}) {
         this.x = x;
         this.y = y;
         this.vx = vx;
@@ -11,6 +11,7 @@ export default class Sprite {
         this.cena = null;
         this.mx = 0;
         this.my = 0;
+        this.controlar = controlar;
     }
     desenhar(ctx) {
         ctx.fillStyle = this.color;
